0f82252deed44e08c48928e1886e27f2293a5e16
Toby1219 <150697833+Toby1219@users.noreply.github.com>
2023-12-02 20:06:59 +0100

    Update settings.py

--- jobber/settings.py
+++ jobber/settings.py
@@ -16,7 +16,7 @@ NEWSPIDER_MODULE = "jobber.spiders"
 # USER_AGENT = "jobber (+http://www.yourdomain.com)"
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = False
+ROBOTSTXT_OBEY = True
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 # CONCURRENT_REQUESTS = 32
